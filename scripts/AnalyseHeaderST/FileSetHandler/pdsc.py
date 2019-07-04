99b637f9414692788447de194aa0fb72ba2f26a0
Julien FAUCHER <julien.faucher@gmx.fr>
2019-07-04 20:29:39 +0200

    Cleanup and comment

--- scripts/AnalyseHeaderST/FileSetHandler/pdsc.py
+++ scripts/AnalyseHeaderST/FileSetHandler/pdsc.py
@@ -9,7 +9,16 @@ logger = logging.getLogger()
 
 class PDSCFile :
 	def __init__(self,filepath : str):
+		"""
+		This class will handle a PDSC file from Keil and extract the mapping between chips names, defines
+		and svd files
 		
+		Nothing have to be done. Only access the result through
+		 - self.define_to_svd
+		 - self.define_to_chip_mapping
+		
+		:param filepath: Path to the .pdsc file
+		"""
 		self.file = filepath
 		self.root : ET.Element = self.cache_and_remove_ns(filepath)
 		
@@ -37,26 +46,34 @@ class PDSCFile :
 	def process(self):
 		
 		for family in self.root.findall("devices/family/subFamily") :
+			#For each recognized "meaningful" family
+			#Debug and compile tag can be found at subFamily... or might not !
 			svd = family.find("debug")
 			define = family.find("compile")
 			
-			
+			#Compile tag might not contain the define attrib at subFamily level.
+			#In this case, skip it.
 			if define is not None and "define" not in define.attrib.keys():
 				define = None
-			
 			
 			for chip in family.findall("device") :
 				logger.debug(f'Checking {chip.attrib["Dname"]}')
 				
+				#If the debug tag wasn(t previously found, that's because it is within the chip
+				#Or because it is not even here.
 				if svd is None :
+					#If nowhere to be found, skip the whole definition
 					if chip.find("debug") is None :
 						logger.warning(f'\tNo association for {chip.attrib["Dname"]:12s}')
 						continue
+					#Else, take the definition from the device.
 					svd_name = chip.find("debug").attrib["svd"]
 				else :
 					svd_name = svd.attrib["svd"]
 				
+				#If the chip define wasn't found, we will try to have it from the chip
 				if define is None :
+					#If nowhere to be found, skip the whole definition
 					if chip.find("compile") is None :
 						logger.warning(f'\tNo header declaration for {chip.attrib["Dname"]:12s}')
 						continue
@@ -65,10 +82,14 @@ class PDSCFile :
 					define_name = define.attrib["define"]
 					
 				chip_name = chip.attrib["Dname"]
+				
+				#Compute the mapping
+				#First : add the chip to the list of defines. Therefore define -> list[chip]
 				if define_name not in self.define_to_chip_mapping :
 					self.define_to_chip_mapping[define_name] = list()
 				self.define_to_chip_mapping[define_name].append(chip_name)
 				
+				#Second : map SVD to define name.
 				if define_name in self.define_to_svd and svd_name != self.define_to_svd[define_name] :
 					logger.error(f"\tMultiple SVD for {define_name}. "
 								 f"Replacing {self.define_to_svd[define_name]} by {svd_name}.")
