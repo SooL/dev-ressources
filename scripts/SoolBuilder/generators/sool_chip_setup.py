84953ecc53d72280ff2e707c03914a079f96f209
Julien FAUCHER <julien.faucher@gmx.fr>
2019-10-09 19:01:50 +0200

    Add licence and total count of defines

--- scripts/SoolBuilder/generators/sool_chip_setup.py
+++ scripts/SoolBuilder/generators/sool_chip_setup.py
@@ -1,9 +1,15 @@
 from structure import ChipSet
 
+
 def generate_sool_chip_setup() :
-	out : str = ""
+	out: str = ""
+
+	with open("license_header.txt","r") as license:
+		out = license.read() + "\n\n"
+
 	max_in_line = 3
-	for family in ChipSet.reference_chipset.families :
+	family_list = sorted(ChipSet.reference_chipset.families.keys())
+	for family in family_list :
 		out += f"//{'#':#^78s}\n"
 		out += f"//#{family:^76s}#\n"
 		out += f"//{'#':#^78s}\n\n"
@@ -31,5 +37,20 @@ def generate_sool_chip_setup() :
 				f"    #define {family}\n"
 				f"#endif\n")
 
+	out += f"#define __SOOL__NB_TOT "
+	curr_line = 0
+	for family in family_list :
+			if curr_line == 0:
+				out += "\\\n         "
+			out += f"__SOOL_NB_{family} + "
+			curr_line += 1
+			if curr_line > max_in_line:
+				curr_line = 0
+	out = out[:out.rfind("+") - 1] + "\n"
+	out += f"#if __SOOL_NB_TOT > 1 \n"
+	out += f"\t#error Only one chip can be defined\n"
+	out += f"#elif __SOOL_NB_TOT == 0\n"
+	out += f"\t#error At least one chip have to be defined\n"
+	out += "#endif"
 	return out
 
