a80b0129836fc1d6dc5ce8f31d416583a1fb7238
Julien FAUCHER <julien.faucher@gmx.fr>
2019-11-04 22:50:23 +0100

    Add GPIOB_OSPEEDR handling

--- scripts/SoolBuilder/cleaners/register_name_cleaner.py
+++ scripts/SoolBuilder/cleaners/register_name_cleaner.py
@@ -5,9 +5,11 @@ from fnmatch import fnmatch
 def DEFAULT_reg_cleaner(register : "Register", periph_name = None) :
 	pass
 
+
 def GPIO_reg_cleaner(register : "Register", p_name = None):
-	if register.name in ["OSPEEDER"] :
+	if register.name in ["OSPEEDER", "GPIOB_OSPEEDR"] :
 		register.name = "OSPEEDR"
+
 
 # For a given group, provide a proper cleaner function. None is the default one.
 register_association_table : T.Dict[T.Union[None,str],T.Callable] = {
