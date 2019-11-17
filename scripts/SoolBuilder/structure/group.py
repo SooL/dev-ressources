92ff17521acc50f97e39fd74bfb126e3e955cff0
Loic France <loicfrance.developer@gmail.com>
2019-11-17 13:26:18 +0100

    Divide svd peripheral merging in 2 functions
    merging based on strictly identical structure,
    and merging based on identical name

--- scripts/SoolBuilder/structure/group.py
+++ scripts/SoolBuilder/structure/group.py
@@ -98,21 +98,25 @@ class Group(Component) :
 		peripheral.set_parent(self)
 		self.edited = True
 
-	def __merge_svd_peripherals(self, name_based: bool = False):
+	def __struct_based_periph_merge(self) :
 		length = len(self.peripherals)
 		i = 0
-
-		while i < (length - 1):
+		while i < (length - 1) :
 			periph_1 : Peripheral = self.peripherals[i]
 			i += 1
-
-			j = i  # begin at next peripheral
-			while j < length:
+			j = i
+			while j < length :
 				periph_2 : Peripheral = self.peripherals[j]
-				condition = (periph_1.name is not None and periph_1.name == periph_2.name) if name_based\
-					else periph_1.mapping_equivalent_to(periph_2, ignore_templates=False)
+				# Structures are strictly identical
+				same_struct = periph_1.mapping_equivalent_to(periph_2, ignore_templates=False)
+				# If only one mapping per peripheral and mappings are compatibles
+				compatible_mappings = len(periph_1.mappings) == 1 and len(periph_2.mappings) == 1\
+				                      and periph_1.mappings[0].compatible(periph_2.mappings[0])
+				different_names = periph_1.name is not None and\
+				                  periph_2.name is not None and\
+				                  periph_1.name != periph_2.name
 
-				if condition :  # registers, fields are the same.
+				if (same_struct or compatible_mappings) and not different_names :
 					if periph_1.name != periph_2.name :
 						if periph_1.name is None :
 							periph_1.name = periph_2.name
@@ -124,67 +128,86 @@ class Group(Component) :
 							logger.info(f"peripheral {repr(periph_1.instances)} is identical"
 							            f" to unnamed peripheral {repr(periph_2.instances)}."
 							            " Second periph merged into first")
-						elif periph_2.name is not None :
-							logger.error("merged two peripherals with a different name :"
-							                f" {periph_1} ({repr(periph_1.instances)}) and"
-							                f" {periph_2} ({repr(periph_2.instances)})")
+					else :
+						if periph_1.brief != periph_2.brief:
+							logger.warning(f"peripherals {repr(periph_1.instances)} and {repr(periph_2.instances)}"
+							               f" don't have the same description, despite being strictly identical :"
+							               f" {periph_1.brief}, {periph_2.brief}")
+
+						periph_1.intra_svd_merge(periph_2)
+						self.peripherals.pop(j)
+						self.edited = True
+						length -= 1
+				else :
+					if same_struct and different_names :
+						logger.warning(f" {periph_1} ({repr(periph_1.instances)}) and"
+						               f" {periph_2} ({repr(periph_2.instances)})"
+						                " have different names, but strictly the same structure")
+					j += 1
+
+	def __name_base_periph_merge(self) :
+		length = len(self.peripherals)
+		i = 0
+		while i < (length - 1) :
+			periph_1: Peripheral = self.peripherals[i]
+			i += 1
+			if periph_1.name is None :
+				continue
+			j = i
+			while j < length :
+				periph_2: Peripheral = self.peripherals[j]
+				if periph_1.name == periph_2.name :
 					if periph_1.brief != periph_2.brief:
 						logger.warning(f"peripherals {repr(periph_1.instances)} and {repr(periph_2.instances)}"
-						               f" don't have the same description, despite being identical :"
+						               f" don't have the same description, despite having the same name :"
 						               f" {periph_1.brief}, {periph_2.brief}")
-					periph_1.intra_svd_merge(periph_2)
+					periph_1.inter_svd_merge(periph_2)
 					self.peripherals.pop(j)
 					self.edited = True
 					length -= 1
-
-				# If only one mapping per peripheral and mappings are compatibles
-				elif len(periph_1.mappings) == 1 and len(periph_2.mappings) == 1 \
-						and periph_1.mappings[0].compatible(periph_2.mappings[0]) :
-					periph_1.intra_svd_merge(periph_2)
-					self.peripherals.pop(j)
-					self.edited = True
-					length -= 1
-
 				else :
 					j += 1
 
-	def __identify_unnamed(self):
+	def __identify_unnamed(self, final:bool):
 
 		# only 1 peripheral in the group => the peripheral takes the name of the group
 		if len(self.peripherals) == 1 and self.peripherals[0].name is None :
 			self.peripherals[0].name = self.name
 			return
 
-		unnamed = [p for p in self.peripherals if p.name is None]
+		if final :
 
-		for periph in unnamed:
-			if len(periph.instances) == 1:
-				logger.info(f"Name unspecified for single-instance peripheral {periph.instances[0]}"
-				            f" in group {self.name}, where multiple peripheral are left unnamed."
-				            " instance name chosen as peripheral class name.")
-				periph.name = periph.instances[0].name
+			unnamed = [p for p in self.peripherals if p.name is None]
 
-		unnamed = [p for p in unnamed if p.name is None]  # update the 'unnamed' list
+			for periph in unnamed:
+				if len(periph.instances) == 1:
+					logger.info(f"Name unspecified for single-instance peripheral {periph.instances[0]}"
+					            f" in group {self.name}, where multiple peripheral are left unnamed."
+					            f" {periph.instances[0].name} chosen as peripheral class name.")
+					periph.name = periph.instances[0].name
 
-		# if there is still unnamed peripherals in the group, their name cannot be chosen.
-		#  A name helper must be created for them
-		if len(unnamed) != 0:
-			logger.error(f"Peripherals {str(list(p.brief for p in list(unnamed)))}"
-			             f" for chipset {repr(list(unnamed)[0].chips.chips)}"
-			             f" cannot be named.")
+			unnamed = [p for p in unnamed if p.name is None]  # update the 'unnamed' list
+
+			# if there is still unnamed peripherals in the group, their name cannot be chosen.
+			#  A name helper must be created for them
+			if len(unnamed) != 0:
+				logger.error(f"Peripherals {str(list(p.brief for p in list(unnamed)))}"
+				             f" for chipset {repr(list(unnamed)[0].chips.chips)}"
+				             f" cannot be named.")
 
 	def before_svd_compile(self, parent_corrector=root_corrector):
 		super().before_svd_compile(parent_corrector)
+		self.__struct_based_periph_merge() # merge equal structures
 
 	def svd_compile(self):
 		super().svd_compile()
-		self.__merge_svd_peripherals()
 		if not self.edited :
-			self.__merge_svd_peripherals(name_based=True)
+			self.__identify_unnamed(final=False)
+			self.__name_base_periph_merge()
 
 	def after_svd_compile(self):
 		super().after_svd_compile()
-		self.__identify_unnamed()
+		self.__identify_unnamed(final=True)
 		for i in range(0, len(self.peripherals)-1) :
 			for j in range(i+1, len(self.peripherals)) :
 				if self.peripherals[i].name == self.peripherals[j].name :
