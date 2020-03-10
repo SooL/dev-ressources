28e55e30161fa30a45bc56fc0edfd15589e05c0f
Julien FAUCHER <julien.faucher@gmx.fr>
2020-03-10 14:56:18 +0100

    Add enable fields listing sql script.

---
+++ scripts/SoolBuilder/sql/find_enable_bits.sql
@@ -0,0 +1,19 @@
+-- List enable fields, their respective registers and the associated chips.
+-- You can select the group to target in the subquery (alias :grp).
+SELECT f.name as fieldname, r.name as regname, GROUP_CONCAT(DISTINCT c.name) as cname
+FROM registers r
+         INNER JOIN reg_placements rp ON r.id = rp.register_id
+         INNER JOIN peripherals p ON rp.periph_id = p.id
+         INNER JOIN field_reg_placement frp ON rp.id = frp.reg_placement_id
+         INNER JOIN fields f ON frp.field_id = f.id
+         INNER JOIN instances i ON p.id = i.periph_id
+         INNER JOIN inst_chip ic ON i.id = ic.inst_id
+         INNER JOIN chips c ON frp.chip_id = c.id
+WHERE p.name == 'RCC' AND
+ 	f.name IN (
+		SELECT (i.name || 'EN') as enfield FROM instances i
+			INNER JOIN peripherals p ON i.periph_id = p.id
+			INNER JOIN groups g ON p.grp_id = g.id
+		WHERE g.name == :grp
+	)
+GROUP BY f.name, r.name;
